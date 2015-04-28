00dd4ba225328a6227366185cdae11dbad6ac22f
Ratan <ratan.r.sur@gmail.com>
2015-04-28 00:33:57 -0400

    todo: set up raspi gpio

--- Sentry.py
+++ Sentry.py
@@ -1,10 +1,31 @@
 from datetime import datetime
 from pygame import mixer
+from collections import deque
 
 interruption='sounds/interruption.ogg' 
 
-mixer.init()
-mixer.music.load(interruption)
-mixer.music.play(-1)
+class Ambience:
+    def __init__(self):
+        self.noiseArray = deque()
+        self.noisiness = 0
+        self.isNight = False
+    #def sample(self):
+        #self.noiseArray.
+
+class Speaker:
+    def __init__(self):
+        mixer.init()
+        self.track = interruption
+        mixer.music.load(self.track)
+        self.volume = 0
+        mixer.music.play(-1)
+        self.playing = true
+    def updateVolume(self, ambienceObj):
+        self.volume = ambienceObj.noisiness
+
+ambience = Ambience()
+speaker = Speaker()
+
+
 #while 1:
-        #1
+    #1
